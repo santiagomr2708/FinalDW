b972c1d359743c7c0a14052a35f3fae7c7aa9fc9
Hajhosua <josue.gonzalez043@pascualbravo.edu.co>
2023-05-09 14:31:20 -0500

    cambios
    jsps

--- proyectodw/src/pages/Empresa.js
+++ proyectodw/src/pages/Empresa.js
@@ -1,7 +1,20 @@
+import "./Empresa.css";
+
 export const Empresa=()=>{
     return (
         <div>
-          <h1>holaaaa estas en carne</h1>
+          <h1><samp>Carnes</samp></h1>
+
+          <div class="img-gallery">
+
+            <img src="https://th.bing.com/th/id/R.9933a59f8bec34ba67208015361c035d?rik=W3q0PmBSKicEsA&riu=http%3a%2f%2f5b0988e595225.cdn.sohucs.com%2fimages%2f20190610%2f479c05763c28408db5d359e0a0db0ca1.jpeg&ehk=i0Olk9GjWz9fPYJLDy1vKDxUQFwKaCYsdlqyq18xIw4%3d&risl=&pid=ImgRaw&r=0"></img>
+            <img src="https://th.bing.com/th/id/OIP.KIOTxCKhIaihVMmEozyQawHaEJ?pid=ImgDet&w=830&h=465&rs=1"></img>
+            <img src="https://static.pourdebon.com/images/1200-630/d3974292ae0e6ed8937d60d3b53ce1a4/travers_de_porc.jpeg"></img>
+            <img src="https://th.bing.com/th/id/OIP.gq5r5n9Bd2AaHV9k5d2eVAHaFj?pid=ImgDet&rs=1"></img>
+            <img src="https://th.bing.com/th/id/OIP.d2vr-O45O1y1dLf8vwMWDgHaFJ?pid=ImgDet&w=1600&h=1113&rs=1"></img>
+
+
+          </div>
 
     
         </div>
