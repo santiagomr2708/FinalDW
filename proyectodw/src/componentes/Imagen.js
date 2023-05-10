68eb2bd7c4547cc3dfe4b856d02bcf74fae64ebe
Hajhosua <josue.gonzalez043@pascualbravo.edu.co>
2023-05-10 12:22:53 -0500

    Create Imagen.js

---
+++ proyectodw/src/componentes/Imagen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Imagen = (props) => {
+  return (
+    <div>
+        <h1>
+            {props.Titulo}
+        </h1>
+        {props.ancho}
+    </div>
+  )
+}
