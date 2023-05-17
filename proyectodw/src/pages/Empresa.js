16adaf6a8342038b99f4bdb0698e4dfaaf74765c
santiagomr2708 <113460466+santiagomr2708@users.noreply.github.com>
2023-05-17 10:40:32 -0500

    actualizacion empresa

--- proyectodw/src/pages/Empresa.js
+++ proyectodw/src/pages/Empresa.js
@@ -1,18 +1,45 @@
-
+import React, { useState } from "react";
 import "./Empresa.css";
-export const Empresa=()=>{
-    return (
-        <div>
-          <h1><samp>Carnes</samp></h1>
 
-          <div class="img-gallery">
+export const Empresa = () => {
+  const [fulImgBoxVisible, setFulImgBoxVisible] = useState(false);
+  const [fulImgSrc, setFulImgSrc] = useState("");
 
-          
+  const openFulImg = (reference) => {
+    setFulImgBoxVisible(true);
+    setFulImgSrc(reference);
+  };
 
+  const closeImg = () => {
+    setFulImgBoxVisible(false);
+  };
 
-          </div>
+  return (
+    <div>
+      <h1>
+        <samp>Carnes</samp>
+      </h1>
 
-    
-        </div>
-    )
-}   
+      <div className={`ful-img ${fulImgBoxVisible ? "show" : ""}`} id="fulImgBox">
+        <img src={fulImgSrc} id="fulImg" alt="" />
+        <span onClick={closeImg}>X</span>
+      </div>
+
+      <h1>
+        <span>Image Gallery</span>
+      </h1>
+
+      <div className="img-gallery">
+        <img src="imgs/1.jpg" onClick={() => openFulImg("imgs/1.jpg")} alt="" />
+        <img src="imgs/2.jpg" onClick={() => openFulImg("imgs/2.jpg")} alt="" />
+        <img src="imgs/3.jpg" onClick={() => openFulImg("imgs/3.jpg")} alt="" />
+        <img src="imgs/4.jpg" onClick={() => openFulImg("imgs/4.jpg")} alt="" />
+        <img src="imgs/5.jpg" onClick={() => openFulImg("imgs/5.jpg")} alt="" />
+        <img src="imgs/6.jpg" onClick={() => openFulImg("imgs/6.jpg")} alt="" />
+        <img src="imgs/7.jpg" onClick={() => openFulImg("imgs/7.jpg")} alt="" />
+        <img src="imgs/8.jpg" onClick={() => openFulImg("imgs/8.jpg")} alt="" />
+      </div>
+    </div>
+  );
+};
+
